d8fb4823114d3d0a6121e9e704df4d22f47186b4
Your Name <you@example.com>
2023-10-30 20:51:17 +1300

    More Mobile Friendly

--- pages/lighting.js
+++ pages/lighting.js
@@ -10,13 +10,13 @@ const Index = () => {
     const [selectedLight, setSelectedLight] = useState(-0);
 
     const [lights, setLights] = useState([
-        { id: 0, col: 1, row: 1, color: "#000000", noAnimation:false},
-        { id: 1, col: 1, row: 2, color: "#000000", noAnimation:false},
-        { id: 2, col: 2, row: 2, color: "#000000", noAnimation:false},
-        { id: 3, col: 3, row: 2, color: "#000000", noAnimation:false},
-        { id: 4, col: 4, row: 2, color: "#000000", noAnimation:false},
-        { id: 5, col: 5, row: 2, color: "#000000", noAnimation:false},
-        { id: 0, col: 5, row: 1, color: "#000000", noAnimation:false},
+        { id: 0, col: 1, row: 1, color: "#000000", noAnimation: false },
+        { id: 1, col: 1, row: 2, color: "#000000", noAnimation: false },
+        { id: 2, col: 2, row: 2, color: "#000000", noAnimation: false },
+        { id: 3, col: 3, row: 2, color: "#000000", noAnimation: false },
+        { id: 4, col: 4, row: 2, color: "#000000", noAnimation: false },
+        { id: 5, col: 5, row: 2, color: "#000000", noAnimation: false },
+        { id: 0, col: 5, row: 1, color: "#000000", noAnimation: false },
     ]);
 
     function lightClick(light) {
@@ -52,21 +52,27 @@ const Index = () => {
 
             <Aside></Aside>
 
-            <main class="md:ml-64 h-auto pt-20 flex items-center justify-center">
-
-                <div className=" bg-slate-900 w-64 h-64 rounded-2xl flex items-center justify-center mr-10">
-                    <HexColorPicker color={color} onChange={colorChanged} className={`${selectedLight == -1 ? "opacity-40" : ""} transition-all`}/>
-                </div>
-                <div className="grid gap-4 text-black max-w-6xl bg-slate-900 rounded-2xl p-5 grid-cols-5">
-                    {lights.map((light) => (
-                        <div
-                            key={light.id}
-                            onClick={() => lightClick(light.id)}
-                            className={`col-start-${light.col} row-start-${light.row} w-20 h-20 flex items-center justify-center m-5 `}>
-                            <div className={`light ${selectedLight == light.id ? "lightSelected" : ""}` } style={{backgroundColor: light.color}}>
-                            </div>
+            <main class="md:ml-64 h-auto pt-20 flex flex-col items-center justify-center">
+                <div class="w-full md:w-auto flex flex-col md:flex-row items-center justify-center">
+                    <div class="bg-slate-900 w-64 h-64 md:w-auto md:h-auto rounded-2xl flex items-center justify-center md:mr-10 p-10">
+                        <HexColorPicker color={color} onChange={colorChanged} className={`${selectedLight == -1 ? "opacity-40" : ""} transition-all`} />
+                    </div>
+                    <div class="text-black bg-slate-900 rounded-2xl p-5  mt-5 md:mt-0">
+                        <div class="grid gap-4  md:grid-cols-5">
+                            {lights.map((light) => (
+                                <div
+                                    key={light.id}
+                                    onClick={() => lightClick(light.id)}
+                                    class={`sm:w-16 sm:h-16 h-10 w-10 md:w-20 md:h-20 flex items-center justify-center m-2 md:m-5 col-start-${light.col} row-start-${light.row}`}
+                                >
+                                    <div
+                                        class={`light ${selectedLight == light.id ? "lightSelected" : ""}`}
+                                        style={{ backgroundColor: light.color }}
+                                    ></div>
+                                </div>
+                            ))}
                         </div>
-                    ))}
+                    </div>
                 </div>
             </main>
         </div>
